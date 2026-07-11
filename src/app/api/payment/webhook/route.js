8e2c740caa9fe7c185a8b8afd75a82aa92f5d069
David Sliren <bluee.97.berry@gmail.com>
2026-07-11 01:05:11 -0500

    feact(api): Agregar enpoint webhook para agregar los casos exitosos de pago a la base de datos

---
+++ src/app/api/payment/webhook/route.js
@@ -0,0 +1,56 @@
+import { payment } from "@/constants/payment";
+import { Contribute } from "@/models/contribution";
+import { contributionSchemaBanckend } from "@/schemas/contribution";
+
+export const POST = async (req) => {
+  const body = await req.json();
+  if (body.type !== "payment") return Response.json({ ok: true });
+
+  const payId = body?.data.id;
+  if (!payId) return Response.json({ ok: true });
+
+  const pay = await payment.get({ id: payId });
+
+  let userId, purpose;
+
+  try {
+    const extrernalRef = JSON.parse(pay.external_reference || "{}");
+    userId = extrernalRef.userId;
+    purpose = extrernalRef.purpose;
+  } catch {
+    return Response.json({ ok: true });
+  }
+
+  if (pay.status !== "approved") return Response.json({ ok: true });
+
+  const data = {
+    paymentId: payId,
+    method: "pasarela",
+    userId,
+    amount: pay.transaction_amount,
+    status: "confirmado",
+    purpose,
+    date: new Date(pay.date_approved).toISOString(),
+  };
+
+  const result = contributionSchemaBanckend.safeParse(data);
+
+  if (!result.success) {
+    console.error({
+      error: result.error.issues.map((e) => ({
+        path: e.path,
+        message: e.message,
+      })),
+    });
+
+    return Response.json({ ok: true });
+  }
+
+  try {
+    await Contribute.create(result.data);
+    return Response.json({ ok: true });
+  } catch (error) {
+    console.error("webhook: error", error);
+    return Response.json({ ok: true });
+  }
+};

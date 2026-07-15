dbf4a9ee423e749b22e79f8a8001462104f6ff2d
David Sliren <bluee.97.berry@gmail.com>
2026-07-14 21:05:22 -0500

    refactor(form): agregar props name e inputName, para que sea mas general

--- src/components/ui/form/inputs/FieldSelect.jsx
+++ src/components/ui/form/inputs/FieldSelect.jsx
@@ -13,16 +13,18 @@ const defaultOptions = ["...", "...."];
 export const FieldSelect = ({
   registerHook = {},
   fieldValues = defaultOptions,
+  inputName = "relationship",
+  name = "Parentesco",
 }) => {
   return (
     <div className="space-y-2">
       <label className="block text-xs font-semibold text-on-surface-variant tracking-[0.05em] ml-1">
-        Parentesco
+        {name}
       </label>
       <div className="relative">
         <select
           className="w-full bg-surface-container-low  border-1 rounded-2xl px-2 py-2 md:py-4 outline-none transition-all text-primary cursor-pointer"
-          {...registerHook("relationship")}
+          {...registerHook(inputName)}
         >
           {fieldValues.map((value, id) => (
             <FieldOption key={id} value={value} />

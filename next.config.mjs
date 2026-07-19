2bf39fd31550ff78005ae873de957fc344065942
David Sliren <bluee.97.berry@gmail.com>
2026-07-18 19:09:57 -0500

    refactor(config): agregar proxy para el manejo del tunnel, ¡SOLO EN DESAAROLLO!

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  allowedDevOrigins: ["pregnancy-judiciary-specked.ngrok-free.dev"],
 };
 
 export default nextConfig;

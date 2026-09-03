2690f784157c6068debfd75353fc443f1f067ea0
David Sliren <bluee.97.berry@gmail.com>
2026-09-02 19:37:33 -0500

    refactor(Frontend/dashboard): agregar prefecQuery a los usuarios en Layout, quitar scroll por pagina dejar que lo maneje el navegador

--- src/app/(home)/dashboard/layout.jsx
+++ src/app/(home)/dashboard/layout.jsx
@@ -3,6 +3,7 @@ import {
   allPatientQueryOptions,
   mainPatientQueryOptions,
 } from "@/hooks/tanstack/query/useQueryPatient";
+import { userQueryAllOptions } from "@/hooks/tanstack/query/useQueryUser";
 import { getQueryClient } from "@/utils/tanstackQuery-config";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
@@ -12,15 +13,14 @@ export default async function Dashboard({ children }) {
   await Promise.all([
     queryClient.prefetchQuery(allPatientQueryOptions()),
     queryClient.prefetchQuery(mainPatientQueryOptions()),
+    queryClient.prefetchQuery(userQueryAllOptions()),
   ]);
 
   return (
     <div className="h-dvh max-h-vh flex bg-surface overflow-hidden">
       <HydrationBoundary state={dehydrate(queryClient)}>
         <Sidebar />
-        <main className="px-10 pt-7 pb-12 w-full overflow-x-hidden scroll-hiden">
-          {children}
-        </main>
+        <main className="px-10 pt-7 pb-12 w-full ">{children}</main>
       </HydrationBoundary>
     </div>
   );

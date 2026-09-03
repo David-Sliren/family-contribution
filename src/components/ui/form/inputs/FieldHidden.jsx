73f55b8dfa2e8eea3fbb150aaa49e9600bbef11e
David Sliren <bluee.97.berry@gmail.com>
2026-09-02 19:41:25 -0500

    feact(Frontend/UI):  Agregar componente FieldHidden para input que requieran valores por defecto siempre

---
+++ src/components/ui/form/inputs/FieldHidden.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+export const FieldHidden = ({
+  name = "name",
+  inputName = "name",
+  inputType = "text",
+  more = {},
+  registerHook = {},
+  error = {},
+  defaultValue = "",
+}) => {
+  const [isView, setIsView] = useState(false);
+
+  function handleViewPassword() {
+    setIsView(!isView);
+  }
+  return (
+    <div className="space-y-2 absolute top-2 left-2 w-fit">
+      <div className="relative flex items-center w-fit gap-2">
+        <label className="block text-xs font-semibold text-outline/60 tracking-[0.05em] ml-1">
+          {name}:
+        </label>
+        <input
+          className="min-w-62 max-h-fit  bg-surface-container-low border-none rounded-2xl transition-all text-xs bg-amber-300 text-outline/60 font-body"
+          autoComplete="one-time-code"
+          type={inputType}
+          disabled={true}
+          {...registerHook(inputName, { value: defaultValue })}
+          {...more}
+        />
+      </div>
+      {error?.[inputName] && (
+        <p className="ml-2 text-[12px] text-red-500">
+          {error[inputName]?.message}
+        </p>
+      )}
+    </div>
+  );
+};
